2890f56798711be4ec1cf5a93ce2f34502125e58
SilviaAvelar <avelar.silvia@yahoo.com>
2023-12-18 16:48:55 +0000

    todos

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+let person = {
+    nome: 'Silvia Cristina Avelar Pereira',
+    idade: '49',
+    nacionalidade: 'Brasileira,',
+    localidade: 'Belém-do-Pará',
+    estadoCivil: 'casada',
+    esposo: 'Jean Carlos Pereira',
+    hobbies: 'fotografias, viajar, ler, ir a praia e assistir um bom filme.',
+    estudos: 'Estou estudando para ser QA',
+    ferramentas: 'Cypress, Selenium, JIRA',
+    linguagens: 'HTML, CSS, JavaScript',
+    outros: 'API, Swagger, Postman, BDD.',
+    resumo: 'Sou uma pessoa atenciosa, dedicada, prestativa, sempre buscando novos conhecimentos. No momento estou envolvida num Projeto Piloto de teste da Tech Fixe, onde alunos fazem simulação de um dia de trabalho de QA. Assim juntos vamos adquirindo experiências e trocando aprendizado.',
+}
+
+console.log('Chamo-me, ' + person.nome, 'tenho ' + person.idade, 'anos, ' + 'sou ' + person.nacionalidade, 'nascida em ' + person.localidade, 'sou ' + person.estadoCivil, 'com ' + person.esposo, 'Gosto de ' + person.hobbies, 'No momento ' + person.estudos, 'Tenho conhecimento nestas ferramentas: ' + person.ferramentas, 'Aprendendo estas linguagens: ' + person.linguagens, 'E continuo buscando aprendizagem em: ' + person.outros, 'Resumo: ' + person.resumo, )
+
---
+++ variaveisIngles.js
@@ -0,0 +1,18 @@
+let person = {
+    name: 'Silvia Cristina Avelar Pereira',
+    age: '49',
+    nationality: 'Brazilian,',
+    locality: 'Belém-do-Pará',
+    matinalStatus: 'married',
+    husband: 'Jean Carlos Pereira',
+    hobbies: 'Photography, Travel, To read, Go to the beach, Watch a good movie.',
+    estudies: 'I am currently studying to be a QA',
+    tools: 'Cypress, Selenium, JIRA',
+    languages: 'HTML, CSS, JavaScript',
+    others: 'API, Swagger, Postman, BDD.',
+    summary: 'I am an Attentive, Dedicated, Helpful person, always seeking new knowledge. I am currently involved in a Tech Fixe pilot test project, where students simulate a day of QA work. This way, together we will acquire experiences and exchange learning.',
+}
+
+console.log('My name is, ' + person.name, "I'm " + person.age, 'years, ' + "I'm " + person.nationality, 'born in ' + person.locality, "I'm " + person.matinalStatus, 'with ' + person.husband, 'I like ' + person.hobbies, 'At the time ' + person.estudies, 'I have knowledge of these: ' + person.tools, 'Learning these languages: ' + person.languages, 'And I continue to seek learning in: ' + person.others, 'Summary: ' + person.summary, )
+
+
